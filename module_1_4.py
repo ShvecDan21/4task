426136df8b9835b848afb59993f5650fe9793c9c
ShvecDan21 <shvecovd16@gmail.com>
2024-09-24 23:58:33 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input('введите значение: ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+first_char = my_string[0]
+last_char = my_string[-1]
+print(first_char)
+print(last_char)
